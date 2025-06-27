32a0cbe52dee5a0dc22073651bcae39873a6f3be
GranReySapo <123195515+GranReySapo@users.noreply.github.com>
2025-06-27 15:19:57 -0300

    feat: implement authentication context and protected routes; enhance header and navbar with user-specific options; update form components for better validation and user experience

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ import AdminDashboard from './components/dashboard/AdminDashboard';
 import AdminMapPage from './components/pages/AdminMapPage';
 import Dashboard from './components/dashboard/Dashboard';
 import PlaceOrderPage from './components/pages/PlaceOrderPage';
+import ProtectedRoute from './components/auth/ProtectedRoute';
+import { AuthProvider } from './context/AuthContext';
 
 function App() {
   const location = useLocation(); 
@@ -20,21 +22,26 @@ function App() {
 
   return (
     <>
-      <Header/>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/registrarse" element={<RegisterPage />} />
-          <Route path="/user" element={<DashboardPage />}> 
-            <Route path='dashboard' element={<Dashboard/>}/>
-            <Route path='placeOrder' element={<PlaceOrderPage/>}/>
-          </Route>
-          <Route path='/admin' element={<AdminDashboardPage/>}>
-            <Route path='dashboard' element={<AdminDashboard/>}/>
-            <Route path='map' element={<AdminMapPage/>}/>
-          </Route>
-        </Routes>
-      {showFooter && <Footer />}
+      <AuthProvider>
+        <Header/>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/registrarse" element={<RegisterPage />} />
+              <Route path="/user"
+              element={<ProtectedRoute>
+              <DashboardPage />
+              </ProtectedRoute>}>
+                <Route path='dashboard' element={<Dashboard/>}/>
+                <Route path='placeOrder' element={<PlaceOrderPage/>}/>
+              </Route>
+            <Route path='/admin' element={<AdminDashboardPage/>}>
+              <Route path='dashboard' element={<AdminDashboard/>}/>
+              <Route path='map' element={<AdminMapPage/>}/>
+            </Route>
+          </Routes>
+        {showFooter && <Footer />}
+      </AuthProvider>
     </>
   )
 }
